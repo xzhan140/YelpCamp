2d9d801281ac886c7f620ee585b8d651c9aefe7e
Karen Zhang <zhangxinyu.tju@gmail.com>
2018-02-04 07:06:40 +0000

    fix bug

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index")
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+//mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://xzhan140:123456@ds225038.mlab.com:25038/yelpcamp_1988");
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
